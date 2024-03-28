f952a6266f36f5d077213870c695a4f2fb59c603
NATTHALIE BOHM <081210001@faculdade.cefsa.edu.br>
2024-03-28 20:54:32 -0300

    inicio criacao das classes checar Reserva p link do calendario

---
+++ src/java/classes/Reserva.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classes;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author 081210001
+ */
+public class Reserva {
+    public LocalDate inicio;
+    public LocalDate fim;
+}
+
+//https://www.codeproject.com/Articles/732679/HTML-Event-Calendar-Scheduler
